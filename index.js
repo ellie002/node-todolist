56bcf90c6d75dd06cbb88df72564339ac4809813
ellie002 <elis.kanova@seznam.cz>
2022-03-28 13:57:20 +0200

    02 - zacatek stranky

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ const express = require("express");
 
 /* Vytvoření základního objektu serverové aplikace */
 const app = express();
+
 /* Nastavení portu, na němž bude spuštěný server naslouchat */
 const port = 3000;
+
+/* Spuštění webového serveru */
+app.listen(port, () =>{
+    console.log(`Server naslouchá na portu ${port}`);
+});
+
+/* Nastavení portu, na němž bude spuštěný server naslouchat */
+const port = 3000;
+
+/* Identifikace složky obsahující statické soubory klientské části webu */
+app.use(express.static("public"));
 
 /* Spuštění webového serveru */
 app.listen(port, () =>{
